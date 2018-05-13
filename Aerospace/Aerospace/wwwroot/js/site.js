3c3c59fab3c554169fa75dda8b1aed5ccd6e6a09
Kevin <32905353+ktangdev@users.noreply.github.com>
2018-05-14 00:16:32 +0100

    Added a table with static data for temporary view. Added a button to make a ajax call to the OpenSky API which proves to be successful.

--- Aerospace/Aerospace/wwwroot/js/site.js
+++ Aerospace/Aerospace/wwwroot/js/site.js
@@ -1,1 +1,11 @@
 ﻿// Write your JavaScript code.
+$(document).ready(function () {
+    $("#opensky").click(function () {
+        $.ajax({
+            url: "https://opensky-network.org/api/states/all",
+            success: function (result) {
+                alert("success api call");
+            }
+        });
+    });
+});
